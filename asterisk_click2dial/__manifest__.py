e796d68acb23032d78b3e523ac079eabe6f6646e
Sébastien COURATIN <sebastien-couratin@semperco.fr>
2026-09-15 18:10:30 +0200

    [18.0] Port asterisk_click2dial from 17.0

---
+++ asterisk_click2dial/__manifest__.py
@@ -0,0 +1,38 @@
+# Copyright 2010-2021 Akretion France (http://www.akretion.com/)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
+# Copyright 2026 Semper Connect
+# @contributor: Sébastien Couratin <...>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Asterisk connector",
+    "version": "18.0.1.0.0",
+    "category": "Phone",
+    "license": "AGPL-3",
+    "summary": "Asterisk-Odoo connector",
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "contributors": [
+        "Semper Connect",
+        "Sébastien Couratin",
+    ],
+    "maintainers": ["alexis-via"],
+    "website": "https://github.com/OCA/connector-telephony",
+    "depends": ["base_phone"],
+    "external_dependencies": {"python": ["requests"]},
+    "data": [
+        "views/asterisk_server.xml",
+        "views/res_users.xml",
+        "security/ir.model.access.csv",
+        "security/asterisk_security.xml",
+    ],
+    "demo": ["demo/asterisk_click2dial_demo.xml"],
+    "assets": {
+        "web.assets_backend": [
+            "asterisk_click2dial/static/src/scss/*.scss",
+            "asterisk_click2dial/static/src/components/**/*.js",
+            "asterisk_click2dial/static/src/components/**/*.xml",
+        ],
+    },
+    "application": True,
+    "installable": True,
+}
